0f590f83fa9274adf571cf02c0b1831c4c4cf568
SHAKTHI-VEL <shakthivel28203@gmail.com>
2022-09-18 14:08:25 +0530

    context api added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,7 @@ import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@mater
 
 import useStyles from './styles';
 import Form from './Form/Form';
+import List from './List/List';
 
 
 const Main = () => {
@@ -25,7 +26,7 @@ const Main = () => {
                 <CardContent className={classes.cardContent}>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
-                            {/* List */}
+                           <List/>
                         </Grid>
                     </Grid>
 
