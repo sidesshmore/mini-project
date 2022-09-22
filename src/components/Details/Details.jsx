79bd917deec75153387a92ef430c4e7dfc409cad
sidesshmore <mosiddhesh@gmail.com>
2022-09-22 22:08:47 +0530

    chart js done

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 
 import {Card, CardHeader, CardContent, Typography } from '@material-ui/core';
-import {Doughnut} from 'react-chartjs-2';
-
+import { Doughnut } from 'react-chartjs-2';
+import {Chart,Legend, Tooltip, ArcElement} from 'chart.js'
+import useTransactions from '../../useTransactions';
 import useStyles from './styles';
+Chart.register(ArcElement);
+Chart.register(Legend);
+Chart.register(Tooltip);
+
+
+
+
 
 const Details = ({title}) => {
 const classes = useStyles();
-
+const {total, chartData} = useTransactions(title);
   return (
     <Card className={title === 'Income'? classes.income : classes.expense }>
         <CardHeader title= {title}/>
             <CardContent>
-                <Typography variant="h5">₹5000</Typography>
-                {/* <Doughnut data="DATA"/> */}
+                <Typography variant="h5">₹{total}</Typography>
+                <Doughnut data={chartData}/>
             </CardContent>
-
-     
     </Card>
     
   )
