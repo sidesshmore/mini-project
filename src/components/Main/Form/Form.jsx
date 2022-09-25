99988eaea6c0942c3422625f4b0bba01323d3a30
sidesshmore <mosiddhesh@gmail.com>
2022-09-25 12:15:27 +0530

    snackbar added

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -8,6 +8,7 @@ import { useContext } from 'react';
 import { useSpeechContext } from '@speechly/react-client';
 import formatDate from '../../../utils/formatDate';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
+import CustomizedSnackbar from '../../Snackbar/Snackbar'
 
 const initialState = {
     amount: '',
@@ -21,11 +22,15 @@ const Form = () => {
     const [formData, setFormData] = useState(initialState);
     const { addTransaction } = useContext(ExpenseTrackerContext);
     const { segment } = useSpeechContext();
+    const [open, setOpen] = useState(false);
 
     const createTransaction = () => {
         if (Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
         
+        
         const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
+        
+        setOpen(true);
         addTransaction(transaction);
         setFormData(initialState);
     }
@@ -76,6 +81,7 @@ const Form = () => {
 
     return (
         <Grid container spacing={2}>
+          <CustomizedSnackbar open={open} setOpen={setOpen}/>
             <Grid item xs={12}>
                 <Typography align='center' variant='subtitle2' gutterBottom>
                     {segment && segment.words.map((w) => w.value).join(" ")}
