b31cf05def1d0001e71108dd5c8f1b0e9e0a0998
sidesshmore <mosiddhesh@gmail.com>
2022-09-25 12:44:27 +0530

    completed ALMOST

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -24,7 +24,7 @@ const Main = () => {
                       <InfoCard />
                        
                     </Typography>
-                    <Divider />
+                    <Divider className={classes.divider} />
                     <Form/>
                 </CardContent>
 
